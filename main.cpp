2f8cdd605c000027f7cb0b7a42512b8e2d4ae9c2
binhtlu <35886498+binhtlu@users.noreply.github.com>
2018-06-03 17:05:49 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
-#include <thongtin.h>
+#include "thongtin.h"
+#include <string>
 using namespace std;
 
 int main()
 {
 	ThongTin xuat;
-	ThongTin mang[100];
-	int n;
-	cout<<"nhap so nhan vien: ";
-	cin>>n;
-	for(int i = 0; i < n; i++)
-	{
-		
-	} 
+	string a;
+	int b;
+	string c;
+	float d;
+	int e;
+	string f;
+	
+	cout<<"nhap hovaten: ";
+	cin>>a;
+	cout<<"ct: ";
+	cin>>f;
+	cout<<"ngayconh: ";
+	cin>>e;
+	xuat.setHoVaTen(a);
+	cout<<"nhap nam sinh: ";
+	cin>>b;
+	xuat.setNamSinh(b);
+	cout<<"nhap scm: ";
+	cin>>c;
+	xuat.setSCMND(c);
+	cout<<"nhap luong co ban: ";
+	cin>>d;
+	xuat.setLuongCoBan(d);
+	cout<<"ngayconh: ";
+	cin>>e;
+	xuat.setSoNgayCong(e);
+	cout<<"ct: ";
+	cin>>f;
+	xuat.setDonViCT(f);
+	
+	
+	xuat.hienthi();
+	
+	 
 }
